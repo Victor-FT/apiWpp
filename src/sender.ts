04988b6d6dc67af51530967fe55bc002af8da322
Victor Tavares <victortavares@suportetecnologias.com.br>
2023-01-26 20:45:59 -0300

    fix

--- src/sender.ts
+++ src/sender.ts
@@ -53,6 +53,33 @@ class Sender {
 
     private initialize() {
 
+        const options = {
+            folderNameToken: 'tokens', //folder name when saving tokens
+            mkdirFolderToken: '', //folder directory tokens, just inside the venom folder, example:  { mkdirFolderToken: '/node_modules', } //will save the tokens folder in the node_modules directory
+            headless: true, // Headless chrome
+            devtools: false, // Open devtools by default
+            useChrome: true, // If false will use Chromium instance
+            debug: false, // Opens a debug session
+            logQR: true, // Logs QR automatically in terminal
+            browserArgs: ['--no-sandbox', '--disable-setuid-sandbox'], // Parameters to be added into the chrome browser instance
+            disableSpins: true, // Will disable Spinnies animation, useful for containers (docker) for a better log
+            disableWelcome: true, // Will disable the welcoming message which appears in the beginning
+            updates: true, // Logs info updates automatically in terminal
+            autoClose: 60000, // Automatically closes the venom-bot only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
+          }
+
+        const qr = (base64Qr: string, asciiQR: string, attempts: number, urlCode: string | undefined) => {
+            this.qr = { base64Qr, asciiQR, attempts, urlCode: urlCode || "" }
+        }
+
+        const status = (statusSession: string) => {
+            console.log('Status Session: ', statusSession); //return isLogged || notLogged || browserClose || qrReadSuccess || qrReadFail || autocloseCalled || desconnectedMobile || deleteToken || chatsAvailable || deviceNotConnected || serverWssNotConnected || noOpenBrowser || initBrowser || openBrowser || connectBrowserWs || initWhatsapp || erroPageWhatsapp || successPageWhatsapp || waitForLogin || waitChat || successChat
+            // Create session wss return "serverClose" case server for close
+            this.connected = ['isLogged', 'qrReadSuccess', "chatsAvailable"].includes(
+                statusSession
+            )
+        }
+
         const start = (client : Whatsapp) => {
             this.client = client;
             
@@ -61,7 +88,7 @@ class Sender {
             })
         }
 
-        create({session: 'api-artigotas-dev', multidevice: false})
+        create({session: 'api-artigotas-dev', multidevice: false, catchQR: qr, statusFind: status})
             .then((client) => start(client))
             .catch((error) => console.error(error))
     }
